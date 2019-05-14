52dcbe1922daf7939b07835fd6c427e5248567ab
dbobb220 <david.bobb.220@gmail.com>
2019-05-14 13:33:39 -0500

    users on load with DOM additions

--- Assignment/book.js
+++ Assignment/book.js
@@ -1,31 +1,66 @@
 let arrayOfUsers = [];
 let addressBookDom = document.querySelector("#address_book")
 
-class user {
-    constructor(name, picture) {
-        this.name = name;
-        this.pictuer = picture;
-    }
+// class user {
+//     constructor(name, lastName, picture, email, nat, dob, address, ) {
+//         this.name = name;
+//         this.lastName = lastName;
+//         this.picture = picture;
+//         this.email = email;
+//         this.nat = nat;
+//         this.dob = dob;
+//         this.address = address;
+//     }
+// }
+
+window.onload = () => {getUsersOnload()}
+
+const getUsersOnload = () => {
+    fetch('https://randomuser.me/api/?results=50')
+        .then(res => res.json())
+        .then(data => {
+            arrayOfUsers = data.results;
+            postUsers(arrayOfUsers);
+        })
 }
 
-const getUsers = () => {
-    fetch('https://randomuser.me/api/')
-      .then(res => res.json())
-      .then(data => {
-          let name = data.results["0"].name.first;
-          let picture = data.results["0"].picture.medium;
-          arrayOfUsers.push(new user(name, picture));
-          console.log(arrayOfUsers)
-          addUsersToDom(name, picture)
-      })
-  }
-
-const addUsersToDom = (name, picture) => {
-    img = document.createElement('img');
-    img.setAttribute("src", picture);
-    addressBookDom.appendChild(img);
+postUsers = (array) => {
+    array.sort((a, b) => a.name.first < b.name.first ? -1 : 1);
+    array.forEach( (val, idx) => {
+       holderCard = document.createElement('div')
+       holderCard.innerHTML = 
+       `<div>${val.name.first} ${val.name.last}</div>
+        <div>
+            <img src="${val.picture.medium}">
+        </div>
+        <div>
+            <button onclick="moreInfo(${idx})">See More Info</button>
+        </div>
+        `
+        addressBookDom.appendChild(holderCard);
+    });
 }
 
-const consoleUsers = () => {
-console.log(arrayOfUsers)
-}
+
+
+// const getUsers = () => {
+//     fetch('https://randomuser.me/api/')
+//       .then(res => res.json())
+//       .then(data => {
+//           let name = data.results["0"].name.first;
+//           let picture = data.results["0"].picture.medium;
+//           arrayOfUsers.push(new user(name, picture));
+//           console.log(arrayOfUsers.sort((a, b) => a.name < b.name ? -1 : 1));
+//           addUsersToDom(name, picture);
+//       })
+//   }
+
+// TODO delete and opt for array method instead
+// const addUsersToDom = (name, picture) => { 
+//     namePlace = document.createElement('p');
+//     namePlace.innerHTML = name;
+//     img = document.createElement('img');
+//     img.setAttribute("src", picture);
+//     addressBookDom.appendChild(namePlace);
+//     addressBookDom.appendChild(img);
+// }
